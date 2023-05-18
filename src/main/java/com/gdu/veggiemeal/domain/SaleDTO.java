5ef99cd88bc7c1ad3b98d2002d97b810e5471903
bbooom2 <ma22c0@naver.com>
2023-05-18 19:39:12 +0900

    2023-05-18

---
+++ src/main/java/com/gdu/veggiemeal/domain/SaleDTO.java
@@ -0,0 +1,13 @@
+package com.gdu.veggiemeal.domain;
+
+public class SaleDTO {
+	
+	private int saleNo;
+	private Product prodCode;
+	private String saleTitle;
+	private String saleContent;
+	private int salePrice;
+	private Product prodPath;
+	private Product prodImgName;
+
+}
