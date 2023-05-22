46865635e03cbc2fb317dd59a2eb1308cedf477b
o-dim <omiia0407@gmail.com>
2023-05-22 17:01:35 +0900

    query edit

--- src/main/webapp/resources/sql/veggiemeal.sql
+++ src/main/webapp/resources/sql/veggiemeal.sql
@@ -1,7 +1,3 @@
-
-
-
-
 DROP TABLE FAST_ORDER_DETAIL;
 DROP TABLE ORDER_TBL;
 DROP TABLE CART_DETAIL;
@@ -62,74 +58,74 @@ CREATE TABLE PRODUCT (
    PROD_PATH                   VARCHAR2(100 BYTE)   NOT NULL,
    PROD_IMG_NAME               VARCHAR2(100 BYTE)   NOT NULL,
    PROD_ORIGIN_PRICE           NUMBER                   NULL,
-    PROD_PRICE                  NUMBER                  NULL,        -- 추가
-   PROD_STOCK                   NUMBER                   NULL,
-    PROD_COUNT                  NUMBER                  NULL         -- CART_COUNT에서 추가됨
+   PROD_PRICE                  NUMBER                   NULL,
+   PROD_STOCK                  NUMBER                   NULL,
+   PROD_COUNT                  NUMBER                   NULL
 );
 
 -- 상품판매 (판매 원티드 페이지)
 CREATE TABLE SALE (
    SALE_NO                       NUMBER               NOT NULL,
-   PROD_CODE                   NUMBER               NOT NULL,
-   SALE_TITLE                   VARCHAR2(100 BYTE)       NULL,
-   SALE_CONTENT               VARCHAR2(100 BYTE)       NULL,
-   SALE_PRICE                   NUMBER                   NULL,
-   PROD_PATH                   VARCHAR2(100 BYTE)   NOT NULL,
-   PROD_IMG_NAME               VARCHAR2(100 BYTE)   NOT NULL
+   PROD_CODE                   	 NUMBER               NOT NULL,
+   SALE_TITLE                    VARCHAR2(100 BYTE)       NULL,
+   SALE_CONTENT                  VARCHAR2(100 BYTE)       NULL,
+   SALE_PRICE                    NUMBER                   NULL,
+   PROD_PATH                     VARCHAR2(100 BYTE)   NOT NULL,
+   PROD_IMG_NAME                 VARCHAR2(100 BYTE)   NOT NULL
 );
 
 -- 상품이미지 : 상품이미지번호, 상품코드, 경로, 파일명, 메인이미지 ( 대표이미지라면1, 아니면0)
 CREATE TABLE PIMG (
    PIMG_NO                       NUMBER               NOT NULL,
-   PIMG_PATH                   VARCHAR2(100 BYTE)   NOT NULL,
-   PIMG_FILE                   VARCHAR2(50 BYTE)   NOT NULL,
-   PROD_CODE                   NUMBER               NOT NULL,
-    PIMG_MAIN_IMG               NUMBER              NOT NULL        -- 추가
+   PIMG_PATH                   	 VARCHAR2(100 BYTE)   NOT NULL,
+   PIMG_FILE                   	 VARCHAR2(50 BYTE)    NOT NULL,
+   PROD_CODE                   	 NUMBER               NOT NULL,
+   PIMG_MAIN_IMG              	 NUMBER               NOT NULL
 );
 
 -- 회원(탈퇴,휴면)
 CREATE TABLE PERSON (
-   PER_NO                      NUMBER               NOT NULL,
-   PW                           VARCHAR2(20 BYTE)   NOT NULL,
-   PHONENO                       CLOB               NOT NULL,
-   POST_ADDRESS               VARCHAR2(5 BYTE)       NULL,
-   ROAD_ADDRESS               VARCHAR2(100 BYTE)       NULL,
-   JIBUN_ADDRESS               VARCHAR2(100 BYTE)       NULL,
+   PER_NO                      	NUMBER               NOT NULL,
+   PW                           VARCHAR2(100 BYTE)   NOT NULL,
+   PHONENO                      CLOB               	 NOT NULL,
+   POST_CODE               		VARCHAR2(5 BYTE)       	 NULL,
+   ROAD_ADDRESS               	VARCHAR2(100 BYTE)       NULL,
+   JIBUN_ADDRESS               	VARCHAR2(100 BYTE)       NULL,
    DETAIL_ADDRESS               VARCHAR2(100 BYTE)   NOT NULL,
-   EMAIL                       VARCHAR2(20 BYTE)   NOT NULL,
-   GENDER                       VARCHAR2(10 BYTE)   NOT NULL,
-   NAME                       VARCHAR2(20 BYTE)   NOT NULL,
-   JOINED_AT                   DATE                   NULL,
-   ID                           VARCHAR2(20 BYTE)   NOT NULL UNIQUE
+   EMAIL                       	VARCHAR2(20 BYTE)    NOT NULL,
+   GENDER                       VARCHAR2(10 BYTE)    NOT NULL,
+   NAME                       	VARCHAR2(20 BYTE)    NOT NULL,
+   JOINED_AT                   	DATE                   	 NULL,
+   ID                           VARCHAR2(20 BYTE)    NOT NULL UNIQUE
 );
 
 -- 장바구니 : 장바구니번호, 장바구니에담은개수
 CREATE TABLE CART_TBL (
-   CART_NO                       NUMBER               NOT NULL,       -- PK
-    COUNT                       NUMBER              NOT NULL
+    CART_NO                    NUMBER               NOT NULL,
+    COUNT                      NUMBER               NOT NULL
 );
 
 -- 
 CREATE TABLE CART_DETAIL (
-    CART_DETAIL_NO             NUMBER               NOT NULL,       -- PK
-    CART_NO                    NUMBER               NOT NULL,       -- FK
-    PER_NO                     NUMBER               NOT NULL        -- FK
+    CART_DETAIL_NO             NUMBER               NOT NULL,
+    CART_NO                    NUMBER               NOT NULL,
+    PER_NO                     NUMBER               NOT NULL
 );
 
 -- 
 CREATE TABLE ORDER_TBL (
-    ORDER_NO                    NUMBER              NOT NULL,       -- PK
-    PER_NO                      NUMBER              NOT NULL,       -- FK
-    CART_DETAIL_NO              NUMBER              NOT NULL,       -- FK
+    ORDER_NO                    NUMBER              NOT NULL,
+    PER_NO                      NUMBER              NOT NULL,
+    CART_DETAIL_NO              NUMBER              NOT NULL,
     TOTAL                       NUMBER              NOT NULL,
-    FAST_ORDER                  NUMBER              NOT NULL         -- (0 카트디테일불러올것이다, 1이면 (바로)주문된다.) 로그인 전제하에
+    FAST_ORDER                  NUMBER              NOT NULL
 );
 
 -- 바로주문 
 CREATE TABLE FAST_ORDER_DETAIL (
-    O_D_NO                      NUMBER              NOT NULL,       -- PK
-    ORDER_NO                    NUMBER              NOT NULL,       -- FK
-    PROD_CODE                   NUMBER              NOT NULL,       -- FK
+    O_D_NO                      NUMBER              NOT NULL,
+    ORDER_NO                    NUMBER              NOT NULL,
+    PROD_CODE                   NUMBER              NOT NULL,
     COUNT                       NUMBER              NOT NULL
 );
 
@@ -137,85 +133,85 @@ CREATE TABLE FAST_ORDER_DETAIL (
 -- 레시피 이미지 게시판(사진 여러 개)
 CREATE TABLE RIMG (
    RECIPE_NO                   NUMBER               NOT NULL,
-   RIMG_NO                     NUMBER              NOT NULL,
+   RIMG_NO                     NUMBER              	NOT NULL,
    RIMG_PATH                   VARCHAR2(100 BYTE)   NOT NULL,
-   RIMG_ORIGIN_NAME           VARCHAR2(300 BYTE)   NOT NULL,
-   RIMG_FILE_NAME               VARCHAR2(50 BYTE)   NOT NULL
+   RIMG_ORIGIN_NAME            VARCHAR2(300 BYTE)   NOT NULL,
+   RIMG_FILE_NAME              VARCHAR2(50 BYTE)   	NOT NULL
 );
 
 -- 레시피 추천 : 사용자(FK), 레시피 번호(FK)
 CREATE TABLE RECOMMAND (
-    RECOMMAND_NO                NUMBER              NOT NULL,
-   PER_NO                       NUMBER               NOT NULL,
+   RECOMMAND_NO                NUMBER              	NOT NULL,
+   PER_NO                      NUMBER               NOT NULL,
    RECIPE_NO                   NUMBER               NOT NULL
 );
 
 -- QNA
 CREATE TABLE QNA (
    QNA_NO                       NUMBER               NOT NULL,
-   TITLE                       VARCHAR2(100 BYTE)   NOT NULL,
-   CONTENT                       CLOB                   NULL,
-   WRITE_AT                   TIMESTAMP               NULL,
-    ID                          VARCHAR2(20 BYTE)   NOT NULL UNIQUE  
+   TITLE                       	VARCHAR2(100 BYTE)   NOT NULL,
+   CONTENT                      CLOB                   	 NULL,
+   WRITE_AT                   	TIMESTAMP                NULL,
+   ID                          	VARCHAR2(20 BYTE)    NOT NULL UNIQUE  
 );
 
 -- QNA
 CREATE TABLE QNA_ANSWER (
    TITLE                       VARCHAR2(100 BYTE)   NOT NULL,
-   CONTENT                       CLOB                   NULL,
-   ANSWER_AT                   TIMESTAMP               NULL,
-    ID                          VARCHAR2(20 BYTE)       NOT NULL UNIQUE  
+   CONTENT                     CLOB                   	NULL,
+   ANSWER_AT                   TIMESTAMP               	NULL,
+   ID                          VARCHAR2(20 BYTE)    NOT NULL UNIQUE  
 );
 
 -- 탈퇴 (탈퇴한 아이디로 재가입이 불가능)
 CREATE TABLE LEAVE_PERSON (
-    LEAVE_PER_NO                NUMBER              NOT NULL,
-   ID                           VARCHAR2(20 BYTE)   NOT NULL UNIQUE,
+   LEAVE_PER_NO                NUMBER              NOT NULL,
+   ID                          VARCHAR2(20 BYTE)   NOT NULL UNIQUE,
    EMAIL                       VARCHAR2(20 BYTE)       NULL,
-   JOINED_AT                   DATE                   NULL,
-   LEAVE_AT                   DATE                   NULL
+   JOINED_AT                   DATE                    NULL,
+   LEAVE_AT                    DATE                    NULL
 );
 
 -- 휴먼 계정
 CREATE TABLE SLEEP_PERSON (
-    SLEEP_PER_NO                NUMBER              NOT NULL,
+   SLEEP_PER_NO                	NUMBER              NOT NULL,
    ID                           VARCHAR2(20 BYTE)   NOT NULL UNIQUE,
-   PW                           VARCHAR2(20 BYTE)   NOT NULL,
-   PHONENO                       CLOB               NOT NULL,
-   POST_ADDRESS               VARCHAR2(5 BYTE)       NULL,
-   ROAD_ADDRESS               VARCHAR2(100 BYTE)       NULL,
-   JIBUN_ADDRESS               VARCHAR2(100 BYTE)       NULL,
-   DETAIL_ADDRESS               VARCHAR2(100 BYTE)   NOT NULL,
-   EMAIL                       VARCHAR2(20 BYTE)   NOT NULL,
-   GENDER                       NUMBER               NOT NULL,
-   NAME                       VARCHAR2(20 BYTE)   NOT NULL,
-   JOINED_AT                   DATE                   NULL,
-   SLEPT_AT                   DATE                   NULL
+   PW                           VARCHAR2(100 BYTE)  NOT NULL,
+   PHONENO                      CLOB               	NOT NULL,
+   POST_CODE               	 	VARCHAR2(5 BYTE)       	NULL,
+   ROAD_ADDRESS                	VARCHAR2(100 BYTE)      NULL,
+   JIBUN_ADDRESS               	VARCHAR2(100 BYTE)      NULL,
+   DETAIL_ADDRESS               VARCHAR2(100 BYTE)  NOT NULL,
+   EMAIL                       	VARCHAR2(20 BYTE)   NOT NULL,
+   GENDER                       NUMBER              NOT NULL,
+   NAME                       	VARCHAR2(20 BYTE)   NOT NULL,
+   JOINED_AT                   	DATE                   	NULL,
+   SLEPT_AT                   	DATE                   	NULL
 );
 
 -- 레시피 게시판
 CREATE TABLE RECIPE (
-   RECIPE_NO                   NUMBER               NOT NULL,       -- PK
-   ID                           VARCHAR2(20 BYTE)   NOT NULL UNIQUE,
-   TITLE                       VARCHAR2(100 BYTE)   NOT NULL,
-   TOOL                       VARCHAR2(1000 BYTE)    NULL,
-   CONTENT1                   VARCHAR2(200 BYTE)       NULL,
-   CONTENT2                   CLOB               NOT NULL,
+   RECIPE_NO                  NUMBER              NOT NULL,
+   ID                         VARCHAR2(20 BYTE)   NOT NULL UNIQUE,
+   TITLE                      VARCHAR2(100 BYTE)  NOT NULL,
+   TOOL                       VARCHAR2(1000 BYTE)     NULL,
+   CONTENT1                   VARCHAR2(200 BYTE)      NULL,
+   CONTENT2                   CLOB                NOT NULL,
    WRITE_AT                   TIMESTAMP               NULL,
-   MODIFIED_AT                   TIMESTAMP               NULL
+   MODIFIED_AT                TIMESTAMP               NULL
 );
 
 -- 레시피 댓글 게시판 : 댓글번호, 레시피번호(fk), 작성자(FK), 내용, 작성일
 CREATE TABLE RECIPE_REPLY (
-   REPLY_NO                   NUMBER               NOT NULL,       -- PK
-   CONTENT                       VARCHAR2(1000 BYTE)       NULL,
+   REPLY_NO                   NUMBER               NOT NULL,
+   CONTENT                    VARCHAR2(1000 BYTE)      NULL,
    GROUP_NO                   NUMBER               NOT NULL,
-    GROUP_ORDER                 NUMBER              NOT NULL,
-   DEPTH                       NUMBER                   NULL,
-   WRITE_AT                   DATE                   NULL,
-   RE_WRITE_AT                   DATE                   NULL,
-   ID                           VARCHAR2(20 BYTE)   NOT NULL UNIQUE,
-    RECIPE_NO                   NUMBER              NOT NULL        -- FK
+   GROUP_ORDER                NUMBER               NOT NULL,
+   DEPTH                      NUMBER                   NULL,
+   WRITE_AT                   DATE                     NULL,
+   RE_WRITE_AT                DATE                     NULL,
+   ID                         VARCHAR2(20 BYTE)    NOT NULL UNIQUE,
+   RECIPE_NO                  NUMBER               NOT NULL
 );
 
 --------------------------- PK ---------------------------
